be79a09a6ebf359d411404005a531353a0904054
all <pgodunov@kait20.ru>
2026-02-27 16:08:12 +0300

    Delete Aurora Motors site directory

--- Aurora Motors site/www/footer.php
+++
@@ -1,7 +0,0 @@
-    </main>
-    <footer>
-        <p>Aurora Motors &copy; 2024 - Будущее уже здесь</p>
-        <p class="footer-small">Все автомобили и персонажи вымышлены</p>
-    </footer>
-</body>
-</html>
